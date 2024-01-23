11b85191fe6246c41a86307fcdfcb600443b5ef3
ankita-7972 <ankitakasare20@gmail.com>
2024-01-23 12:21:53 +0530

    web-in-minite

---
+++ app.js
@@ -0,0 +1,115 @@
+var dest=new Date ("sep.19,2024.10:00:00").getTime();
+
+var x=setInterval(function(){
+    var now=new Date().getTime();
+    var diff=dest-now;
+    var days=Math.floor(diff/(1000*60*60*24));
+    console.log(days);
+    var hours=Math.floor((diff%(1000*60*60))/(1000*60*60));
+    console.log(hours);
+    var minutes=Math.floor((diff%(1000*60*60))/(1000*60));
+    console.log(minutes);
+    var seconds=Math.floor((diff%(1000*60))/1000);
+    console.log(seconds);
+
+    document.getElementById("Tday").innerHTML=days
+    document.getElementById("Tday1").innerHTML=hours
+    document.getElementById("Tday2").innerHTML=minutes
+    document.getElementById("Tday3").innerHTML=seconds
+},1000);
+var dest=new Date ("sep.19,2024.10:00:00").getTime();
+
+var x=setInterval(function(){
+    var now=new Date().getTime();
+    var diff=dest-now;
+    var days=Math.floor(diff/(1000*60*60*24));
+    console.log(days);
+    var hours=Math.floor((diff%(1000*60*60))/(1000*60*60));
+    console.log(hours);
+    var minutes=Math.floor((diff%(1000*60*60))/(1000*60));
+    console.log(minutes);
+    var seconds=Math.floor((diff%(1000*60))/1000);
+    console.log(seconds);
+
+    document.getElementById("Tdaytime4").innerHTML=days
+    document.getElementById("Tdaytime5").innerHTML=hours
+    document.getElementById("Tdaytime6").innerHTML=minutes
+    document.getElementById("Tdaytime7").innerHTML=seconds
+},1000);
+
+
+
+var dest=new Date ("sep.19,2024.10:00:00").getTime();
+
+var x=setInterval(function(){
+    var now=new Date().getTime();
+    var diff=dest-now;
+    var days=Math.floor(diff/(1000*60*60*24));
+    console.log(days);
+    var hours=Math.floor((diff%(1000*60*60))/(1000*60*60));
+    console.log(hours);
+    var minutes=Math.floor((diff%(1000*60*60))/(1000*60));
+    console.log(minutes);
+    var seconds=Math.floor((diff%(1000*60))/1000);
+    console.log(seconds);
+
+    document.getElementById("Tdaytime").innerHTML=days
+    document.getElementById("Tdaytime1").innerHTML=hours
+    document.getElementById("Tdaytime2").innerHTML=minutes
+    document.getElementById("Tdaytime3").innerHTML=seconds
+},1000);
+var dest=new Date ("sep.19,2024.10:00:00").getTime();
+
+var x=setInterval(function(){
+    var now=new Date().getTime();
+    var diff=dest-now;
+    var days=Math.floor(diff/(1000*60*60*24));
+    console.log(days);
+    var hours=Math.floor((diff%(1000*60*60))/(1000*60*60));
+    console.log(hours);
+    var minutes=Math.floor((diff%(1000*60*60))/(1000*60));
+    console.log(minutes);
+    var seconds=Math.floor((diff%(1000*60))/1000);
+    console.log(seconds);
+
+    document.getElementById("Ttime4").innerHTML=days
+    document.getElementById("Ttime5").innerHTML=hours
+    document.getElementById("Ttime6").innerHTML=minutes
+    document.getElementById("Ttime7").innerHTML=seconds
+},1000);
+var dest=new Date ("sep.19,2024.10:00:00").getTime();
+var x=setInterval(function(){
+    var now=new Date().getTime();
+    var diff=dest-now;
+    var days=Math.floor(diff/(1000*60*60*24));
+    console.log(days);
+    var hours=Math.floor((diff%(1000*60*60))/(1000*60*60));
+    console.log(hours);
+    var minutes=Math.floor((diff%(1000*60*60))/(1000*60));
+    console.log(minutes);
+    var seconds=Math.floor((diff%(1000*60))/1000);
+    console.log(seconds);
+
+    document.getElementById("Ttime").innerHTML=days
+    document.getElementById("Ttime1").innerHTML=hours
+    document.getElementById("Ttime2").innerHTML=minutes
+    document.getElementById("Ttime3").innerHTML=seconds
+},1000);
+var dest=new Date ("sep.19,2024.10:00:00").getTime();
+var x=setInterval(function(){
+    var now=new Date().getTime();
+    var diff=dest-now;
+    var days=Math.floor(diff/(1000*60*60*24));
+    console.log(days);
+    var hours=Math.floor((diff%(1000*60*60))/(1000*60*60));
+    console.log(hours);
+    var minutes=Math.floor((diff%(1000*60*60))/(1000*60));
+    console.log(minutes);
+    var seconds=Math.floor((diff%(1000*60))/1000);
+    console.log(seconds);
+
+    document.getElementById("Tdtime").innerHTML=days
+    document.getElementById("Tdtime1").innerHTML=hours
+    document.getElementById("Tdtime2").innerHTML=minutes
+    document.getElementById("Tdtime3").innerHTML=seconds
+},1000);
